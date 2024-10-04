5aaeb5a49bea3d66bcfa4bf04a846d3ebc19dbaa
sharonhan97 <114363588+sharonhan97@users.noreply.github.com>
2024-10-03 23:09:27 -0700

    Delete auth_src directory

--- auth_src/routes/__test__/signout.test.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import { app } from '../../app';
-
-it('returns a 200 on successful signout', async()=>{
-    return request(app)
-        .get('/api/users/signout')
-        .send({})
-        .expect(200);
-});
