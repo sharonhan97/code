a9d4a7398e7bc1f922d53c46cf3ca38b97be26e0
sharonhan97 <114363588+sharonhan97@users.noreply.github.com>
2024-10-03 23:09:56 -0700

    Delete tickets_src directory

--- tickets_src/events/ticket-created-publisher.ts
+++
@@ -1,6 +0,0 @@
-import {Publisher,Subjects, TicketCreatedEvent} from '@shanticket/common';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent>{
-    readonly subject =  Subjects.TicketCreated;
-    
-}
--- tickets_src/events/ticket-updated-publisher.ts
+++
@@ -1,6 +0,0 @@
-import {Publisher,Subjects, TicketUpdatedEvent} from '@shanticket/common';
-
-export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent>{
-    readonly subject =  Subjects.TicketUpdated;
-    
-}
